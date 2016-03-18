05b08727e02496809105c6a611d028133764425a
onyoo <roberto9m9@gmail.com>
2016-03-18 14:41:17 +0100

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -9,9 +9,16 @@ function SomeDirective() {
 		controller: function() {
 			this.text = 'Replace this text!';
 		},
+		require: 'someDirective',
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+			var realElement = elem[0];
 
+			var span = realElement.querySelector('span');
+			realElement.addEventListener('click', function () {
+				span.innerText = 'Thank you for clicking!';
+				scope.$apply();
+			});
 		}
 	}
 }
