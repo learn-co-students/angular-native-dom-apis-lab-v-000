6dd9918c13b320bbac05f5557e02d4a998e0b69b
febbraiod <donedesign1@gmail.com>
2016-05-14 10:31:57 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -6,14 +6,23 @@ function SomeDirective() {
 				'<span>Click here to replace the text</span>',
 			'</div>'
 		].join(''),
+		require: 'someDirective',
 		controller: function() {
 			this.text = 'Replace this text!';
 		},
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+			element = elem[0];
+			headline = element.querySelector('h3');
+			clickable = element.querySelector('span');
 
+			addEventListener('click', function () {
+				headline.innerHTML = "Thank you for clicking!";
+
+				scope.$apply();
+			});
 		}
-	}
+	};
 }
 
 angular
