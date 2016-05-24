349b20e683798e5a59dc2bd3be5da881e16a7311
shinyrachel <rachel.kory@gmail.com>
2016-05-24 11:38:11 -0400

    complete

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -10,8 +10,15 @@ function SomeDirective() {
 			this.text = 'Replace this text!';
 		},
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
-
+		require: 'someDirective',
+		link: function (scope, elem, attrs, ctrl) {
+			debugger;
+			var span = elem[0].querySelector('span');
+			span.addEventListener("click", function(){
+				debugger;
+				ctrl.text = "Thank you for clicking!";
+				scope.$apply();
+			});
 		}
 	}
 }
