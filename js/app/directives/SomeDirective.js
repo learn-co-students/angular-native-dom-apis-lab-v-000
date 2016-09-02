ba4e4fb2e594c420f8ab3a402a0f738962c3c477
agdavid <antonio.g.david@gmail.com>
2016-09-02 13:25:26 -0400

    add clarifying comments

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -9,14 +9,14 @@ function SomeDirective() {
 		controller: function() {
 			this.text = 'Replace this text!';
 		},
-		require: 'someDirective',
-		controllerAs: 'some',
+		require: 'someDirective', //access this controller
+		controllerAs: 'some', //use alias to permit use of 'this' instead of '$scope'
 		link: function (scope, elem, attrs, ctrl) {
-			var actualElement = elem[0];
-			var spanElement = actualElement.querySelector('span');
-			spanElement.addEventListener('click', function() {
-				ctrl.text = "Thank you for clicking!";
-				scope.$apply();
+			var actualElement = elem[0]; //grab actual template element
+			var spanElement = actualElement.querySelector('span'); //grab specific element within
+			spanElement.addEventListener('click', function() { //add event listener
+				ctrl.text = "Thank you for clicking!"; //update property value
+				scope.$apply(); //apply to scope similar to ng-click
 			});
 		}
 	}
