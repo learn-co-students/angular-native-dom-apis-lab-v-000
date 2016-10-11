3d6f8eacf9dfc7392a330968282f692647a63003
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-10 19:25:01 -0700

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -9,8 +9,17 @@ function SomeDirective() {
 		controller: function() {
 			this.text = 'Replace this text!';
 		},
+		// require: 'someDirective',
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+
+			var element = elem[0];
+			var spanElement = element.querySelector('span');
+
+			spanElement.addEventListener('click', function() {
+				ctrl.text = 'Thank you for clicking!';
+				scope.$apply();
+			});
 
 		}
 	}
