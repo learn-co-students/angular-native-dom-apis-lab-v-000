bb02bc65eab5c4ae8ec00c0a2b7faeffdc7bf3e1
shoko furushita <shktkt219@gmail.com>
2016-11-10 13:59:39 -0500

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -6,12 +6,19 @@ function SomeDirective() {
 				'<span>Click here to replace the text</span>',
 			'</div>'
 		].join(''),
+		require: 'someDirective',
 		controller: function() {
 			this.text = 'Replace this text!';
 		},
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+       var el = elem[0];
+			 var span = el.querySelector('span');
+			 span.addEventListener('click', function() {
+				 ctrl.text = 'Thank you for clicking!';
 
+				 scope.$apply();
+			 });
 		}
 	}
 }
