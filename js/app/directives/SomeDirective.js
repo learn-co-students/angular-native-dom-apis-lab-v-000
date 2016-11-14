e7a2b983513c10621450f4f0e167b4936c392d0a
auranbuckles <auran59@gmail.com>
2016-11-14 14:47:11 -0500

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -10,8 +10,16 @@ function SomeDirective() {
 			this.text = 'Replace this text!';
 		},
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+			var element = elem[0];
 
+			var spanElement = element.querySelector('span');
+
+			spanElement.addEventListener('click', function() {
+				ctrl.text = "Thank you for clicking!";
+
+				scope.$apply();
+			});
 		}
 	}
 }
